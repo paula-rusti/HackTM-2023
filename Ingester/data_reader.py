b4c45c1f6fb16781b64f7b9e29c611bd6c997090
Denis-Cosmin NUTIU <dnutiu@bitdefender.com>
2023-05-13 18:14:42 +0300

    update data reader

--- Ingester/data_reader.py
+++ Ingester/data_reader.py
@@ -1,8 +1,10 @@
 import asyncio
 import json
+import os
+import time
 
 import aio_pika
-
+from influxdb_client import InfluxDBClient, Point
 from persistance.data_formatter import format_data
 from persistance.influx_client import write_api
 from persistance.mongo_client import MongoClient
@@ -10,16 +12,26 @@ from persistance.mongo_client import MongoClient
 
 async def consume():
     # todo pass clients to constructor
-    # mongo_client_instance = MongoClient()
-    # await mongo_client_instance.create_collection()
+    mongo_client_instance = MongoClient()
+    await mongo_client_instance.create_collection()
 
     # Connect to the RabbitMQ server
-    connection = await aio_pika.connect_robust('amqp://guest:guest@localhost/')
+    connection_string = os.getenv("RABBIT_MQ") or 'amqp://guest:guest@localhost/'
+    connection = await aio_pika.connect_robust(connection_string)
     channel = await connection.channel()
 
     # Declare the queue and bind to it
-    queue_name = 'sensor_data_queue'
-    queue = await channel.declare_queue(queue_name)
+    queue_name = 'bme680-data'
+    queue = await channel.declare_queue(queue_name, passive=True)
+
+    # influx db
+    username = 'username'
+    password = 'password'
+
+    database = 'telegraf'
+    retention_policy = 'autogen'
+
+    bucket = f'{database}/{retention_policy}'
 
     async with queue.iterator() as queue_iter:
         async for message in queue_iter:
@@ -27,12 +39,13 @@ async def consume():
                 print("Received message:", message.body)
                 data_for_influx = json.loads(message.body)
                 formatted_data = format_data(data_for_influx)
-                write_api.write(bucket="initial_bucket", org="Home", record=formatted_data['temperature'])
-                write_api.write(bucket="initial_bucket", org="Home", record=formatted_data['humidity'])
-                write_api.write(bucket="initial_bucket", org="Home", record=formatted_data['pressure'])
-                write_api.write(bucket="initial_bucket", org="Home", record=formatted_data['gas_resistance'])
-                # await mongo_client_instance.insert_one(json.loads(message.body))
-
+                with InfluxDBClient(url='http://localhost:8086', token=f'{username}:{password}', org='-') as client:
+                    with client.write_api() as write_api:
+                        write_api.write(bucket="enviro", org="enviro", record=formatted_data['temperature'])
+                        write_api.write(bucket="enviro", org="enviro", record=formatted_data['humidity'])
+                        write_api.write(bucket="enviro", org="enviro", record=formatted_data['pressure'])
+                        write_api.write(bucket="enviro", org="enviro", record=formatted_data['gas_resistance'])
+                await mongo_client_instance.insert_one(json.loads(message.body))
 
 async def main():
     # Start the consumer
